86b2344d447bcad93c0241a47ecb8bf786110f67
tomashina <tomislav@agmedia.hr>
2024-04-11 15:56:03 +0200

    header trans

--- resources/lang/hr/front/apartment.php
+++ resources/lang/hr/front/apartment.php
@@ -13,65 +13,11 @@ return [
     |
     */
 
-    'reserve_title' => 'Rezervirajte svoj smještaj',
-    'view_gallery' => 'Pogledajte cijelu galeriju',
-    'checkin_title' => 'Prijava -> Odjava',
-    'additional_apartment_options' => 'Dodatne opcije',
-    'adults_title' => 'Odraslih (+12g)',
-    'children_title' => 'Djece (3-11g)',
-    'baby_title' => 'Bebe (0-2g)',
-    'reserve_btn_title' => 'Rezerviraj',
-    'guests' => 'osoba',
-    'rooms' => 'soba',
-    'beds' => 'kreveta',
-    'order_default' => 'Sortiraj',
-    'order_oldest' => 'Najstarije',
-    'order_top' => 'Top ocjene',
-    'order_popular' => 'Popularno',
-    'order_newest' => 'Najnovije',
-    'search_title' => 'Pretraži',
-    'search_box_title' => 'Pretraži dostupnost',
-    'description_title' => 'Opis',
-    'offer_title' => 'Što ovo mjesto nudi',
-    'show_title' => 'Prikaži sve pogodnosti',
-    'prices_title' => 'Cijene',
-    'price_regular' => 'Cijena za dane u tjednu',
-    'price_additional_person' => 'Cijena dodatne osobe',
-    'price_cleaning' => 'Cijena naknade za čišćenje',
-    'price_weekends' => 'Cijena za vikend (Petak & Subota)',
-    'calendar_title' => 'Kalendar dostupnosti',
-    'where_title' => 'Gdje ste smješteni',
-    'things_title' => 'Dobro je znati',
-    'select_city' => 'Odaberite grad',
-    'html_block' => '<div class="row ">
-                    <div class="col-md-4 mb-4">
-                      <h5 class="text-secondary">Kućna pravila</h5>
-                           <ul>
-
-                                                    <li><i class="far fa-clock font-13 text-primary me-1"></i> Odjava: 10:00 (0 €)</li>
-                                                     <li><i class="far fa-clock font-13 text-primary me-1"></i> Odjava: 13:00 (30€)</li>
-                                                    <li><i class="fas fa-door-closed font-13 text-primary me-1"></i> Samostalna prijava šifrom </li>
-                                                    <li><i class="fas fa-smoking-ban font-13 text-primary me-1"></i> Zabranjeno pušenje</li>
-                                                    <li><i class="fas fa-smoking-ban font-13 text-primary me-1"></i> Zabranjeni kućni ljubimci</li>
-                                                    <li><i class="fas fa-users-slash font-13 text-primary me-1"></i> Zabranjeno održavanje zabava</li>
-                                                </ul>
-                     </div>
-                     <div class="col-md-4 mb-4">
-                                                <h5 class="text-secondary">Zdravlje & sigurnost</h5>
-                                                <ul>
-                                                    <li><i class="fas fa-shield-virus font-13 text-primary me-1"></i> Primjenjuju se sigurnosne prakse za COVID-19</li>
-                                                    <li><i class="fas fa-certificate font-13 text-primary me-1"></i> Alarm za ugljični monoksid </li>
-                                                    <li><i class="fas fa-certificate font-13 text-primary me-1"></i> Alarm za dim </li>
-
-                                                </ul>
-                                            </div>
-                     <div class="col-md-4 ">
-                                                <h5 class="text-secondary">Politika otkazivanja</h5>
-                                                <ul>
-                                                   <li>Besplatno otkazivanje u bilo kojem trenutku ukoliko apartman popunimo novom rezervacijom.</li>
+    'home' => 'Naslovnica',
+    'vozni_red' => 'Pogledajte cijelu galeriju',
+    'o_nama' => 'O nama',
+    'faq' => 'Faq',
+    'kontakt' => 'Kontakt',
 
 
-                                                </ul>
-                                            </div>
-    </div>',
 ];
