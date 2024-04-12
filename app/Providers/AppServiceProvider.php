e4d6f88d083a34802685223d9a42c02106bee3e2
Filip Jankoski <filip@agmedia.hr>
2024-04-12 11:52:41 +0200

    _fix: pages

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Back\Settings\Settings;
+use App\Models\Front\Catalog\Page;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\View;
@@ -25,6 +26,9 @@ class AppServiceProvider extends ServiceProvider
     {
         $mode = Settings::getCached('app', 'mode');
         View::share('mode', $mode);
+
+        $uvjeti = Page::query()->where('subgroup', 'Uvjeti kupnje')->get();
+        View::share('uvjeti_kupnje', $uvjeti);
         
         Paginator::useBootstrap();
     }
