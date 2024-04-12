0b3b4b3ad35a98f61736b50bc35dbdc50e8d848c
tomashina <tomislav@agmedia.hr>
2024-04-12 11:43:02 +0200

    fix faq

--- resources/views/front/page.blade.php
+++ resources/views/front/page.blade.php
@@ -25,48 +25,33 @@
 
 @section('content')
 
-    <div class=" bg-white pt-4 pb-3">
-        <div class="container d-lg-flex justify-content-between py-2 py-lg-3">
+    <div class=" bg-white pt-4 mt-4 pb-3">
+        <div class="container mt-5 d-lg-flex justify-content-between py-2 py-lg-3">
             <div class="order-lg-2 mb-3 mb-lg-0 pt-lg-2">
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb breadcrumb-light flex-lg-nowrap justify-content-center justify-content-lg-start">
                         <li class="breadcrumb-item"><a class="text-nowrap" href="{{ route('index') }}"><i class="ci-home"></i>{{ __('front/common.home') }}</a></li>
-                        <li class="breadcrumb-item text-nowrap active" aria-current="page">{{ __('front/common.faq') }}</li>
+                        <li class="breadcrumb-item text-nowrap active" aria-current="page">{{ $page->translation()->title }}</li>
                     </ol>
                 </nav>
             </div>
             <div class="order-lg-1 pe-lg-4 text-center text-lg-start">
-                <h1 class="h3 text-dark">{{ __('front/common.faq') }}</h1>
+                <h1 class="h3 text-dark">{{ $page->translation()->title }}</h1>
             </div>
         </div>
     </div>
     <div class="full-row bg-white pt-3 pb-5">
         <div class="container">
             <div class="row">
-                <div class="col-lg-3">
-                    <div class="navigation_link_widget mb-5 bg-gray p-4">
-                        <h5 class="double-down-line-left text-secondary position-relative pb-4 mb-4">{{ __('front/common.additional_info') }}</h5>
-                        <ul>
-                            @foreach($pages as $page)
-                                <li>
-                                    <a href="{{ route('page', ['page' => $page->translation(current_locale())->slug]) }}">{{ $page->translation(current_locale())->title }}</a>
-                                </li>
-                            @endforeach
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-9">
-                    <div class="info-pages bg-gray px-4 py-5 ">
-                        @foreach ($faqs as $fa)
-                            <div class="faq-item mb-4"><span class="faq-question bg-primary text-white">Q</span>
-                                <div class="d-table">
-                                    <h5 class="mb-2 text-secondary">{{ $fa->title }}</h5>
-                                    {!! $fa->description !!}
-                                    <hr>
-                                </div>
 
-                            </div>
-                        @endforeach
+                <div class="col-lg-12">
+
+
+
+                                    {!! $page->translation()->description !!}
+
+
+
                     </div>
                 </div>
             </div>
